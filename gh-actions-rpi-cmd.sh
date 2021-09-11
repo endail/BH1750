caf4f8dcafdcde938cae6640a1ce064127a5fd44
endail <52652357+endail@users.noreply.github.com>
2021-09-12 03:56:00 +1000

    Update gh-actions-rpi-cmd.sh

--- gh-actions-rpi-cmd.sh
+++ gh-actions-rpi-cmd.sh
@@ -4,4 +4,4 @@ export VIRTUAL_PI=1
 apt-get update -y && apt-get full-upgrade -y
 ./install-deps.sh
 
-g++ -Wall -o prog tests/test.cpp -I include -o test -llgpio
+g++ -Wall -o prog tests/test.cpp src/BH1750.cpp -I include -o test -llgpio
